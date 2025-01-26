2588af65c95727f7d4b2294b42dc69b342e7d615
vishnukbhasker <107983290+vishnukbhasker@users.noreply.github.com>
2025-01-26 10:34:56 +0530

    schemas.py

---
+++ backend/app/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    name: str
+    age: int
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
